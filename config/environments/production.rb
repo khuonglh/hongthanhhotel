051460d2e0a4f0ab9d89801be7a27ed5c4ce0f52
Khuong Luu <khuonglh@gmail.com>
2016-12-25 22:07:09 +0700

    fix GA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,4 +97,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # replace this with your tracker code
+  GA.tracker = ENV["GA_TRACKER"]
 end
